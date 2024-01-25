bc63086ee6b064c8886c4015fca3e51d45312d3e
anderson.wagner <anderson.wagner@roadcard.com.br>
2024-01-25 17:01:11 -0300

    correção da leitura da forma de pagamento preferencial

--- src/main/java/br/com/fiap/postech/linkingpark/controller/FormaPagamentoController.java
+++ src/main/java/br/com/fiap/postech/linkingpark/controller/FormaPagamentoController.java
@@ -2,6 +2,12 @@ package br.com.fiap.postech.linkingpark.controller;
 
 import br.com.fiap.postech.linkingpark.dto.FormaPagamentoDTO;
 import br.com.fiap.postech.linkingpark.service.FormaPagamentoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,6 +23,12 @@ public class FormaPagamentoController {
     public FormaPagamentoController(FormaPagamentoService formaPagamentoService) {
         this.formaPagamentoService = formaPagamentoService;
     }
+    @Operation(summary = "lista as formas de pagamentos validas para informar no cadastro de motorista")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Sucesso - aqui estão as formas de pagamento suportadas",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = FormaPagamentoDTO.class)) }),
+    })
     @GetMapping
     public ResponseEntity<List<FormaPagamentoDTO>> findAll() {
         List<FormaPagamentoDTO> formaPagamentos = formaPagamentoService.findAll();
--- src/main/java/br/com/fiap/postech/linkingpark/controller/MotoristaController.java
+++ src/main/java/br/com/fiap/postech/linkingpark/controller/MotoristaController.java
@@ -3,6 +3,8 @@ package br.com.fiap.postech.linkingpark.controller;
 import br.com.fiap.postech.linkingpark.dto.MotoristaDTO;
 import br.com.fiap.postech.linkingpark.dto.VeiculoDTO;
 import br.com.fiap.postech.linkingpark.service.MotoristaService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -10,56 +12,64 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
 @RestController
 @RequestMapping("/motorista")
-public class MotoristaContoller {
+public class MotoristaController {
     private final MotoristaService motoristaService;
 
     @Autowired
-    public MotoristaContoller(MotoristaService motoristaService) {
+    public MotoristaController(MotoristaService motoristaService) {
         this.motoristaService = motoristaService;
     }
 
+    @Operation(summary = "obtém todos os motoristas")
     @GetMapping
     public ResponseEntity<List<MotoristaDTO>> findAll() {
         List<MotoristaDTO> motoristas = motoristaService.findAll();
         return ResponseEntity.ok(motoristas);
     }
-
+    @Operation(summary = "obtém um motorista")
     @GetMapping("/{id}")
     public ResponseEntity<MotoristaDTO> findById(@PathVariable Long id) {
         MotoristaDTO motorista = motoristaService.findById(id);
         return ResponseEntity.ok(motorista);
     }
-
+    @Operation(summary = "insere um motorista")
     @PostMapping
     public ResponseEntity<MotoristaDTO> save(@Valid @RequestBody MotoristaDTO motoristaDTO) {
         MotoristaDTO savedMotorista = motoristaService.save(motoristaDTO);
         return new ResponseEntity<>(savedMotorista, HttpStatus.CREATED);
     }
 
+    @Operation(summary = "altera os dados de um motorista")
     @PutMapping("/{id}")
     public ResponseEntity<MotoristaDTO> update(@PathVariable Long id, @Valid @RequestBody MotoristaDTO motoristaDTO) {
         MotoristaDTO updatedMotorista = motoristaService.update(id, motoristaDTO);
         return ResponseEntity.ok(updatedMotorista);
     }
 
+    @Operation(summary = "remove um motorista")
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(@PathVariable Long id) {
         motoristaService.delete(id);
         return ResponseEntity.noContent().build();
     }
+
+    @Operation(summary = "adiciona um veículo a um motorista")
     @PutMapping("/{id}/veiculo")
     public ResponseEntity<Void> update(@PathVariable Long id, @Valid @RequestBody VeiculoDTO veiculoDTO) {
         motoristaService.adicionaVeiculo(id, veiculoDTO);
         return ResponseEntity.noContent().build();
     }
+
+    @Operation(summary = "remove um veículo de um motorista")
     @DeleteMapping("/{id}/veiculo/{placa}")
     public ResponseEntity<Void> delete(@PathVariable Long id, @PathVariable String placa) {
         motoristaService.deleteVeiculo(id, placa);
         return ResponseEntity.noContent().build();
     }
+
+    @Operation(summary = "lista os veículos de um motorista")
     @GetMapping("/{id}/veiculo")
     public ResponseEntity<List<VeiculoDTO>> findAllVeiculos(@PathVariable Long id) {
         List<VeiculoDTO> veiculos = motoristaService.findAllVeiculos(id);
